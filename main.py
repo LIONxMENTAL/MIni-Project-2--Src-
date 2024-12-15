47d7c7b3f927746e6c6deb3e6345a89d5b0198a0
Himanshu <anshmaurya2828@gmail.com>
2024-12-15 17:09:36 +0530

    second

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ n = random.randint(1,100)
 a = -1
 Guesses = 0
 while(a != n):
-    Guesses += 1
     a = int(input("Guess a Number: "))
     if( a > n):
         print("Choose a lower number")
-    else:
+    elif(a<n)
         print("Choose a higher number")
+    Guesses += 1
 
 print(f"You have guessed the correct number {n} in {Guesses} guesses")
 
